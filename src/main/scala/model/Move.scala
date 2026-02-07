fce4c46ceab9103c82d5aece3ef0c9c4ec0e6ef9
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2026-02-07 15:52:35 +0100

    aggiunto metodo isCapture a trait Move per identificare mosse che sono delle catture

--- src/main/scala/model/Move.scala
+++ src/main/scala/model/Move.scala
@@ -4,13 +4,20 @@ package model
 /**
  * Represents a move from one position to another.
  */
-trait Move{
+trait Move:
   def from: Square
   def to: Square
   def player: Player
   def captured: Option[Square]
   def isPromotion: Boolean
-}
+
+  /**
+   * Checks if the move involves capturing an opponent's piece.
+   *
+   * @return true if there is a captured square, false otherwise.
+   */
+  def isCapture: Boolean = captured.isDefined
+
 
 /**
  * The class that implement the Move trait.
