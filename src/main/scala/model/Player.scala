ea48de85b6a056b38064bdc28d431ad9e21d4268
Viola Leonardo <leonardo.viola2@studio.unibo.it>
2026-02-14 15:25:07 +0100

    aggiunti commenti

--- src/main/scala/model/Player.scala
+++ src/main/scala/model/Player.scala
@@ -4,18 +4,23 @@ import model.enums.ColorType
 
 /**
  * Base trait for representing a player in the game.
- * @param name the player's name
- * @param color the color assigned to the player
  */
 sealed trait Player:
   def name: String
   def color: ColorType
- 
- 
+
+
+/**
+ * Represents a human participant in the game.
+ * @param name  The display name chosen by the user.
+ * @param color The color assigned to the player (LIGHT or DARK), which also determines the starting turn and direction.
+ */
 case class HumanPlayer(override val name: String, override val color: ColorType) extends Player
- 
- /**
- * An AI-controlled player.
- **/
-case class AIPlayer(override val color: ColorType,override val name: String = "AI") extends Player
-  
+
+/**
+ * Represents an AI-controlled opponent.
+ * @param color The color assigned to the AI.
+ * @param name  The name of the AI player, defaults to "AI".
+ * This class identifies the player as an artificial entity, allowing the controller to trigger automated move calculations.
+ */
+case class AIPlayer(override val color: ColorType, override val name: String = "AI") extends Player
